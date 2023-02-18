747184e12c521dace90151aaea1896fb4fd412c0
DHRUVINKUMAR BHATT <75667686+dhruvinkumar@users.noreply.github.com>
2023-02-19 01:15:28 +0530

    Add files via upload

---
+++ Remove duplicate elements from sorted Array.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        // code here
+        map<int,int> m;
+        for(int i=0;i<n;i++){
+            m[a[i]]++;
+        }
+        map<int,int> ::iterator it;
+        int i=0;
+        for(it=m.begin();it!=m.end();it++){
+            a[i]=it->first;
+            i++;
+        }
+        return m.size();
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        int a[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>a[i];
+        }
+    Solution ob;
+    int n = ob.remove_duplicate(a,N);
+
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    }
+}
+// } Driver Code Ends
