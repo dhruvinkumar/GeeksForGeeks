021a8eb4bf2ad9477fa8ee452d5be232ccc0ef12
DHRUVINKUMAR BHATT <75667686+dhruvinkumar@users.noreply.github.com>
2023-03-14 21:25:42 +0530

    Add files via upload

---
+++ First and last occurrences of x.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+vector<int> find(int arr[], int n , int x )
+{
+    // code here
+    vector<int> v;
+    int l=-1,r=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==x){
+            if(l==-1){l=i;}
+            
+            r=i;
+        }
+    }
+    v.push_back(l);
+    v.push_back(r);
+    return v;
+}
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        ans=find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
